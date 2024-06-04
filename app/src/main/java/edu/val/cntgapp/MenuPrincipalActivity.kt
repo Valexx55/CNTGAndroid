ab9bd118feacbecaa89a39ca1fd104b57f4c0370
valexx55 <valexx55@gmail.com>
2024-06-04 17:05:39 +0200

    Menú Lateral Completado

--- app/src/main/java/edu/val/cntgapp/MenuPrincipalActivity.kt
+++ app/src/main/java/edu/val/cntgapp/MenuPrincipalActivity.kt
@@ -1,5 +1,6 @@
 package edu.`val`.cntgapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
@@ -7,7 +8,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import edu.`val`.cntgapp.adivina.AdivinaActivity
+import edu.`val`.cntgapp.creditos.CreditosActivity
+import edu.`val`.cntgapp.fechayhora.SeleccionFechaYHoraActivity
+import edu.`val`.cntgapp.imc.IMCActivity
+import edu.`val`.cntgapp.intents.BusquedaActivity
+import edu.`val`.cntgapp.perros.PerrosActivity
+import edu.`val`.cntgapp.productos.ProductosActivity
 import edu.`val`.cntgapp.util.Constantes
+import edu.`val`.cntgapp.versiones.VersionesActivity
+import edu.`val`.cntgapp.webview.WebActivity
 
 class MenuPrincipalActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -47,9 +57,26 @@ class MenuPrincipalActivity : AppCompatActivity(), NavigationView.OnNavigationIt
         return super.onOptionsItemSelected(item)
     }
 
-    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
-        Log.d(Constantes.ETIQUETA_LOG, "Opcion ${p0.order} tocada")
+    override fun onNavigationItemSelected(opcionMenu: MenuItem): Boolean {
+        Log.d(Constantes.ETIQUETA_LOG, "Opcion ${opcionMenu.order} tocada")
         //TODO definir los intents según la opción, lanzando la actividad correspondiente
+
+        var objetoClass: Class<*>? = null
+        when (opcionMenu.order)
+        {
+            0 -> {objetoClass = VersionesActivity::class.java }
+            1 -> {objetoClass = WebActivity::class.java }
+            2 -> {objetoClass = CreditosActivity::class.java }
+            3 -> {objetoClass = IMCActivity::class.java }
+            4 -> {objetoClass = BusquedaActivity::class.java }
+            5 -> {objetoClass = SeleccionFechaYHoraActivity::class.java }
+            6 -> {objetoClass = AdivinaActivity::class.java }
+            7 -> {objetoClass = PerrosActivity::class.java }
+            8 -> {objetoClass = ProductosActivity::class.java }
+
+        }
+        var intent = Intent(this,objetoClass )
+        startActivity(intent)
         this.drawerLayout.closeDrawers()//cierro el menú sea cual sea la opcion tocada
         return true
     }
